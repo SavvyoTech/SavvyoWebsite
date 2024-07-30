f936f8a1a61b98eee93786fbb5ce06cd741e4a9c
justary27 <76696648+justary27@users.noreply.github.com>
2024-07-30 15:39:04 +0530

    chore: Add initial project files and configurations

---
+++ src/app/components/header.tsx
@@ -0,0 +1,11 @@
+import { AppBar } from "@mui/material";
+
+const Header = () => {
+    return (
+        <AppBar position="static">
+            Savyo
+        </AppBar>
+    );
+};
+
+export default Header;
