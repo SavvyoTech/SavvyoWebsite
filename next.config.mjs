e5fcabe30e3b09d08e8be823b562f73ca0ddb5bf
justary27 <76696648+justary27@users.noreply.github.com>
2024-10-01 01:55:31 +0530

    deploy on gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.IS_PROD === 'true';
+
+const nextConfig = {
+    assetPrefix: isProd ? '/SavvyoWebsite/': '',
+    trailingSlash: true,
+    output: 'export',
+};
 
 export default nextConfig;
